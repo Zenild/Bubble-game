c7b8ff9349ff67a97878db91cf8a6131c71161bb
Yusuf <71589435+Yusufibin@users.noreply.github.com>
2024-07-06 16:05:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Variables du jeu
+// Variables du jeu.
 let canvas = document.getElementById('game-canvas');
 let ctx = canvas.getContext('2d');
 let player = { x: 100, y: 100, speedX: 0, speedY: 0, radius: 10 };
