dc5df3e1d5b28bfc6d43085a3ad5eea71f2e6ac9
Yusuf <71589435+Yusufibin@users.noreply.github.com>
2024-08-30 11:24:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ let player = { x: 100, y: 100, speedX: 0, speedY: 0, radius: 20 };
 let enemies = [];
 let bombs = [];
 let stars = [];
+let clockUps = [];
+let miniBombs = [];
 let score = 0;
 let timer = 60;
+let gameSpeed = 1;
 
 let music = new Audio('bubbleSong.mp3');
 const musicDuration = 180 * 1000; 
@@ -18,6 +21,10 @@ let lastBombSpawn = 0;
 let bombSpawnInterval = 10000;
 let lastStarSpawn = 0;
 let starSpawnInterval = 12000; 
+let lastClockUpSpawn = 0;
+let clockUpSpawnInterval = 15000;
+let lastMiniBombSpawn = 0;
+let miniBombSpawnInterval = 8000;
 
 let playerImage = new Image();
 playerImage.src = 'player.png';
@@ -33,6 +40,12 @@ bombImage.src = 'bomb.png';
 
 let starImage = new Image();
 starImage.src = 'star.png';
+
+let clockUpImage = new Image();
+clockUpImage.src = 'horloge_up.png';
+
+let miniBombImage = new Image();
+miniBombImage.src = 'minibombe.png';
 
 let resourcesLoaded = false;
 let lastTime = 0;
@@ -113,6 +126,32 @@ function drawStars() {
   }
 }
 
+function drawClockUps() {
+  for (let i = 0; i < clockUps.length; i++) {
+    if (clockUpImage.complete) {
+      ctx.drawImage(clockUpImage, clockUps[i].x, clockUps[i].y, 30, 30);
+    } else {
+      ctx.beginPath();
+      ctx.rect(clockUps[i].x, clockUps[i].y, 20, 20);
+      ctx.fillStyle = 'blue';
+      ctx.fill();
+    }
+  }
+}
+
+function drawMiniBombs() {
+  for (let i = 0; i < miniBombs.length; i++) {
+    if (miniBombImage.complete) {
+      ctx.drawImage(miniBombImage, miniBombs[i].x, miniBombs[i].y, 20, 20);
+    } else {
+      ctx.beginPath();
+      ctx.rect(miniBombs[i].x, miniBombs[i].y, 15, 15);
+      ctx.fillStyle = 'orange';
+      ctx.fill();
+    }
+  }
+}
+
 function draw() {
   if (backgroundImage.complete) {
     ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
@@ -122,6 +161,8 @@ function draw() {
   drawEnemies();
   drawBombs();
   drawStars();
+  drawClockUps();
+  drawMiniBombs();
 
   ctx.font = '24px Arial';
   ctx.fillStyle = 'black';
@@ -129,11 +170,14 @@ function draw() {
   ctx.textBaseline = 'top';
   ctx.fillText(`Score: ${score}`, 10, 10);
   ctx.fillText(`Temps restant: ${Math.floor(timer)}`, 10, 30);
+  ctx.fillText(`Vitesse: ${gameSpeed.toFixed(2)}x`, 10, 50);
 }
 
 function update(deltaTime) {
-  player.x += player.speedX * (deltaTime / 16);
-  player.y += player.speedY * (deltaTime / 16);
+  gameSpeed = 1 + (60 - timer) / 120; // Augmentation progressive de la vitesse
+
+  player.x += player.speedX * (deltaTime / 16) * gameSpeed;
+  player.y += player.speedY * (deltaTime / 16) * gameSpeed;
 
   if (player.x + player.radius > canvas.width) {
     player.x = canvas.width - player.radius;
@@ -147,7 +191,7 @@ function update(deltaTime) {
   }
 
   for (let i = 0; i < enemies.length; i++) {
-    enemies[i].x += 2 * (deltaTime / 16);
+    enemies[i].x += 2 * (deltaTime / 16) * gameSpeed;
     if (enemies[i].x > canvas.width) {
       enemies.splice(i, 1);
     }
@@ -160,12 +204,12 @@ function update(deltaTime) {
     }
   }
 
-  if (Math.random() < 0.05 * (deltaTime / 16)) {
+  if (Math.random() < 0.05 * (deltaTime / 16) * gameSpeed) {
     enemies.push({ x: 0, y: Math.random() * canvas.height });
   }
 
   let now = Date.now();
-  if (now - lastBombSpawn > bombSpawnInterval) {
+  if (now - lastBombSpawn > bombSpawnInterval / gameSpeed) {
     let bombY = Math.random() * canvas.height;
     for (let i = 0; i < 3; i++) {
       bombs.push({ x: 0, y: bombY });
@@ -174,7 +218,7 @@ function update(deltaTime) {
   }
 
   for (let i = 0; i < bombs.length; i++) {
-    bombs[i].x += 2 * (deltaTime / 16);
+    bombs[i].x += 2 * (deltaTime / 16) * gameSpeed;
     if (bombs[i].x > canvas.width) {
       bombs.splice(i, 1);
     }
@@ -187,13 +231,13 @@ function update(deltaTime) {
     }
   }
 
-  if (now - lastStarSpawn > starSpawnInterval) {
+  if (now - lastStarSpawn > starSpawnInterval / gameSpeed) {
     stars.push({ x: 0, y: Math.random() * canvas.height });
     lastStarSpawn = now;
   }
 
   for (let i = 0; i < stars.length; i++) {
-    stars[i].x += 2 * (deltaTime / 16);
+    stars[i].x += 2 * (deltaTime / 16) * gameSpeed;
     if (stars[i].x > canvas.width) {
       stars.splice(i, 1);
     }
@@ -203,6 +247,44 @@ function update(deltaTime) {
     if (distance(player.x, player.y, stars[i].x, stars[i].y) < player.radius + 15) {
       score += 5;
       stars.splice(i, 1);
+    }
+  }
+
+  if (now - lastClockUpSpawn > clockUpSpawnInterval / gameSpeed) {
+    clockUps.push({ x: 0, y: Math.random() * canvas.height });
+    lastClockUpSpawn = now;
+  }
+
+  for (let i = 0; i < clockUps.length; i++) {
+    clockUps[i].x += 2 * (deltaTime / 16) * gameSpeed;
+    if (clockUps[i].x > canvas.width) {
+      clockUps.splice(i, 1);
+    }
+  }
+
+  for (let i = 0; i < clockUps.length; i++) {
+    if (distance(player.x, player.y, clockUps[i].x, clockUps[i].y) < player.radius + 15) {
+      timer += 10;
+      clockUps.splice(i, 1);
+    }
+  }
+
+  if (now - lastMiniBombSpawn > miniBombSpawnInterval / gameSpeed) {
+    miniBombs.push({ x: 0, y: Math.random() * canvas.height });
+    lastMiniBombSpawn = now;
+  }
+
+  for (let i = 0; i < miniBombs.length; i++) {
+    miniBombs[i].x += 3 * (deltaTime / 16) * gameSpeed; // Les mini-bombes sont plus rapides
+    if (miniBombs[i].x > canvas.width) {
+      miniBombs.splice(i, 1);
+    }
+  }
+
+  for (let i = 0; i < miniBombs.length; i++) {
+    if (distance(player.x, player.y, miniBombs[i].x, miniBombs[i].y) < player.radius + 10) {
+      score -= 2;
+      miniBombs.splice(i, 1);
     }
   }
 
@@ -229,7 +311,7 @@ function endGame() {
   }).then((result) => {
     if (result.isConfirmed) {
       let playerName = result.value;
-      saveScore(playerName, finalScore); // Utilise finalScore au lieu de score
+      saveScore(playerName, finalScore);
     }
     timer = 60;
     init();
@@ -242,15 +324,13 @@ function saveScore(playerName, finalScore) {
     return;
   }
 
-  // Encodez le nom du joueur et le score pour l'URL
   const encodedPlayerName = encodeURIComponent(playerName);
-  const encodedScore = encodeURIComponent(finalScore); // Utilise finalScore
+  const encodedScore = encodeURIComponent(finalScore);
 
-  // Construisez l'URL pour une opération SET de Redis
   const url = `https://charmed-slug-43732.upstash.io/set/${encodedPlayerName}/${encodedScore}`;
 
   fetch(url, {
-    method: 'GET', // Redis KV sur Upstash utilise GET pour les opérations
+    method: 'GET',
     headers: {
       'Authorization': 'Bearer AarUAAIjcDE3ZGZmOWFlYWMzM2Q0ZTYyYTY0NzExZGM0YjI4ZmVmY3AxMA',
     }
@@ -274,20 +354,26 @@ function saveScore(playerName, finalScore) {
 function init() {
   score = 0;
   timer = 60;
+  gameSpeed = 1;
   enemies = [];
   bombs = [];
   stars = [];
+  clockUps = [];
+  miniBombs = [];
   player.x = canvas.width / 2;
   player.y = canvas.height / 2;
   player.speedX = 0;
   player.speedY = 0;
   lastBombSpawn = 0;
   lastStarSpawn = 0;
+  lastClockUpSpawn = 0;
+  lastMiniBombSpawn = 0;
   music.play(); 
 }
 
 function checkResourcesLoaded() {
-  if (playerImage.complete && enemyImage.complete && backgroundImage.complete && bombImage.complete && starImage.complete) {
+  if (playerImage.complete && enemyImage.complete && backgroundImage.complete && 
+      bombImage.complete && starImage.complete && clockUpImage.complete && miniBombImage.complete) {
     resourcesLoaded = true;
     init();
     gameLoop();
@@ -299,6 +385,8 @@ enemyImage.onload = checkResourcesLoaded;
 backgroundImage.onload = checkResourcesLoaded;
 bombImage.onload = checkResourcesLoaded;
 starImage.onload = checkResourcesLoaded;
+clockUpImage.onload = checkResourcesLoaded;
+miniBombImage.onload = checkResourcesLoaded;
 
 function gameLoop(currentTime) {
   if (!lastTime) lastTime = currentTime;
