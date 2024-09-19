33dc528c0b1392358cbee3fdfd67ca8bf2951cf6
Zenild <142363133+Zenild@users.noreply.github.com>
2024-09-19 13:41:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ function drawPlayer() {
 function drawEnemies() {
   for (let i = 0; i < enemies.length; i++) {
     if (enemyImage.complete) {
-      ctx.drawImage(enemyImage, enemies[i].x, enemies[i].y, 40, 40);
+      ctx.drawImage(enemyImage, enemies[i].x, enemies[i].y, 22, 22);
     } else {
       ctx.beginPath();
       ctx.rect(enemies[i].x, enemies[i].y, 20, 20);
